05359d75aa0103695644e7607558f149b6cc4e69
paulMuema <pkmuema87@gmail.com>
2022-09-11 16:46:24 +0300

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - prints alphabet in lowercase
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char p[26] = "abcdefghijklmnopqrstuvwxyz";
+	int x;
+
+	for (x = 0; x < 26; x++)
+	{
+		putchar(p[26]);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 0-positive_or_negative.c - prints wheter a number stored in variable n is positive or negative.
 1-last_digit.c - prints last digit of a number stored in variable n.
+2-print_alphabet.c - prints alphabet in lowercase.
 
