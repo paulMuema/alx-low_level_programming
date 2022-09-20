8f65b40ae55b50bd3acac1a32af1ca330000cbb8
paulMuema <pkmuema87@gmail.com>
2022-09-20 22:48:38 +0300

    Task 1 Success

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *swap_int - swaps values of two integers
+  *@a : integer value one
+  *@b : integer value two
+  *
+  *Return: no return
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
