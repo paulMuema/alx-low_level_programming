af04ab49d1d179d311345f8d657a0ed7096912c5
paulMuema <pkmuema87@gmail.com>
2022-10-04 23:52:21 +0300

    String Duplicate Task Success

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *_strdup - returns pointer to newly allocated space in memory.
+  *@str: string.
+  *
+  *Return: pointer of an array of characters
+  */
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int a, b;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (a = 0; str[a] != '\0'; a++)
+		;
+
+	p = (char *)malloc(sizeof(char) * (a + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+		p[b] = str[p];
+
+	return (p);
+}
