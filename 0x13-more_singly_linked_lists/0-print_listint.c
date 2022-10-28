1546d8320ac23b88d488c774732452d51f0c9ced
paulMuema <pkmuema87@gmail.com>
2022-10-28 03:40:12 +0300

    print elements task success

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *print_listint - prints all elements of a list
+  *@h: head of the linked list
+  *
+  *Return: number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
